b9e1a444e6c990d52189193420a8ae702fde886b
Ivan Henriques da Silva <ivanhenriquessilva@gmail.com>
2020-05-12 20:22:38 -0300

    Basic Data and Products Layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:virtual_store/tabs/home_tab.dart';
+import 'package:virtual_store/tabs/products_tab.dart';
 import 'package:virtual_store/widgets/custom_drawer.dart';
 
 
@@ -18,8 +19,18 @@ class HomeScreen extends StatelessWidget {
         //Container(color: Colors.green,),
         Scaffold(
           body: HomeTab(),
-          drawer: CustomDrawer(),
-        )
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        ),
+        Container(color: Colors.yellow,),
+        Container(color: Colors.green,),
       ],
     );
   }
