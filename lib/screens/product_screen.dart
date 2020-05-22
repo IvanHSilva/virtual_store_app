e14337868d95cecba52f574b92ab9c60a207a132
Ivan Henriques da Silva <ivanhenriquessilva@gmail.com>
2020-05-22 19:37:59 -0300

    Product Class and Screen

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:virtual_store/datas/product_data.dart';
 
@@ -13,12 +14,149 @@ class ProductScreen extends StatefulWidget {
 class _ProductScreenState extends State<ProductScreen> {
 
   final ProductData product;
+  String size;
   _ProductScreenState(this.product);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+
+    final Color primaryColor = Theme.of(context).primaryColor;
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: Carousel(
+              images: product.images.map((url){
+                return NetworkImage(url);
+              }).toList(),
+              dotSize: 4.0,
+              dotSpacing: 15.0,
+              dotBgColor: Colors.transparent,
+              dotColor: primaryColor,
+              autoplay: false,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  product.title,
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  maxLines: 3,
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                Text(
+                  'R\$ ${product.price.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    color: primaryColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                Text('Tamanho',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                SizedBox(
+                  height: 36.0,
+                  child: GridView(
+                    padding: EdgeInsets.symmetric(vertical: 2.0),
+                    scrollDirection: Axis.horizontal,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      mainAxisSpacing: 10.0,
+                      childAspectRatio: 0.5,
+                      ), 
+                    children: product.sizes.map(
+                      (s){
+                        return GestureDetector(
+                          onTap: (){
+                            setState(() {
+                              size = s;
+                            });
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                              border: Border.all(
+                                color: s == size ? primaryColor : Colors.grey[500],
+                                width: 3.0,
+                              ),
+                            ),
+                            width: 150.0,
+                            alignment: Alignment.center,
+                            child: Text(
+                              s,
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.w500,
+                                color: s == size ? primaryColor : Colors.black,
+                              ),
+                            ),
+                          ),
+                        );
+                      }
+                    ).toList(),
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                SizedBox(
+                  height: 44.0,
+                  width: 500.0,
+                  child: RaisedButton(
+                    onPressed: size != null ?
+                    (){} : null,
+                    child: Text('Adicionar ao Carrinho',
+                      style: TextStyle(
+                        fontSize: 18.0
+                      ),
+                    ),
+                    color: primaryColor,
+                    textColor: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text('Descrição:',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(product.description,
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
