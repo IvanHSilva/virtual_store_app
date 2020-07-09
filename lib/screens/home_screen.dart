b57eff20645e7e07b8f9e177b0f7badcba72a814
Ivan Henriques da Silva <ivanhenriquessilva@gmail.com>
2020-07-09 17:35:33 -0300

    Stores and Finish App

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:virtual_store/tabs/home_tab.dart';
 import 'package:virtual_store/tabs/orders_tab.dart';
+import 'package:virtual_store/tabs/places_tab.dart';
 import 'package:virtual_store/tabs/products_tab.dart';
 import 'package:virtual_store/widgets/cart_button.dart';
 import 'package:virtual_store/widgets/custom_drawer.dart';
@@ -33,7 +34,14 @@ class HomeScreen extends StatelessWidget {
           body: ProductsTab(),
           floatingActionButton: CartButton(),
         ),
-        Container(color: Colors.yellow,),
+        Scaffold(
+          appBar: AppBar(
+            title: Text('Lojas'),
+            centerTitle: true,
+          ),
+          body: PlacesTab(),
+          drawer: CustomDrawer(_pageController),
+        ),
         Scaffold(
           appBar: AppBar(
             title: Text('Meus Pedidos'),
